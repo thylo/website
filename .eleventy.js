c0dda4190b50cb76629830e8027115952e693013
Julien Moreau <julien@thylo.be>
2020-05-19 22:29:52 +0200

    Add names

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,12 @@
-// const moment = require("moment");
-// const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-// const now = new Date();
-//
+const friends = require("./src/_data/friends.json");
+
 module.exports = function (eleventyConfig) {
-  //   // blogpost collection
-  //   eleventyConfig.addCollection("blogposts", function(collection) {
-  //     return collection.getFilteredByGlob("./src/blogposts/*.md").filter(item => {
-  //       return item.data.draft !== true && item.date <= now;
-  //     });
-  //   });
-  //
-  //   // projects collection
-  //   eleventyConfig.addCollection("projects", function(collection) {
-  //     return collection.getFilteredByGlob("./src/projects/*.md");
-  //   });
-  //
-  //   // limit filter
-  //   eleventyConfig.addFilter("limit", function(array, limit) {
-  //     return array.slice(0, limit);
-  //   });
-  //
-  //   // date filter
-  //   eleventyConfig.addFilter("date", function(date, format) {
-  //     return moment(date).format(format);
-  //   });
-  //
-  //   // Syntax highlighting (prism)
-  //   eleventyConfig.addPlugin(syntaxHighlight);
-  //
-  //   // pass through
-  //   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
-  //   eleventyConfig.addPassthroughCopy("./src/apple-touch-icon.png");
-  //
-  eleventyConfig.addNunjucksShortcode("friend", function (nickname) {
-    return nickname;
+  eleventyConfig.addShortcode("friend", (slug) => {
+    const friend = friends.find((f) => f.slug === slug);
+    if (!friend) {
+      return `<span style='color:red'>${slug}</span>`;
+    }
+    return `<a href="${friend.link}" target="_blank">${friend.name}</a>`;
   });
 
   // Base config
