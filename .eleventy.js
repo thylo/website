c600d0be3354ce06542b633e6a6599f510ed4f9b
Julien Moreau <julien@thylo.be>
2024-02-23 09:52:51 +0100

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 const friends = require("./src/_data/friends.json");
+const yaml = require("js-yaml");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
-module.exports = function (eleventyConfig) {
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addDataExtension("yaml, yml", (contents) =>
+      yaml.load(contents)
+  );
+
   eleventyConfig.addShortcode("friend", (slug) => {
     const friend = friends.find((f) => f.slug === slug);
     if (!friend) {
@@ -9,12 +15,36 @@ module.exports = function (eleventyConfig) {
     return `<a href="${friend.link}" target="_blank">${friend.name}</a>`;
   });
 
-  // Base config
+
+  // ignores
+  eleventyConfig.ignores.add("src/assets/**/*");
+  eleventyConfig.watchIgnores.add("src/assets/**/*");
+
+  // passthrough copy
+  eleventyConfig.setServerPassthroughCopyBehavior("copy");
+  eleventyConfig.addPassthroughCopy({ "./src/static": "/" });
+  //eleventyConfig.addPassthroughCopy("./src/assets/img");
+  eleventyConfig.addPassthroughCopy("./src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("./src/assets/svg");
+
+  // server config
+  eleventyConfig.setServerOptions({
+    watch: ["./dist/assets/css/**/*.css", "./dist/assets/js/**/*.js"],
+    port: 3000,
+  });
+
+
+
+  // base config
   return {
-    passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "dist",
+      includes: "_includes",
+      data: "_data",
     },
+    templateFormats: ["liquid", "md", "yml", "jmo"],
+    htmlTemplateEngine: "liquid",
+    markdownTemplateEngine: "liquid",
   };
 };
